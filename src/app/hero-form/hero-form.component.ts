fb57aac437409983506d2e6a4724f35c69fe15ab
Tom Na Thalang <tom.na-thalang@Beslogic.com>
2019-04-30 17:12:25 -0400

    add localstorage

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
+import { getLocaleNumberSymbol } from "@angular/common";
 
 @Component({
   selector: "app-hero-form",
@@ -13,13 +14,17 @@ export class HeroFormComponent implements OnInit {
 
   submitted = false;
 
+  data: Hero[] = [];
+
   areaValue: number | null;
-  unitValue: string | undefined;
+  unitValue: string | undefined = "Feet";
   widthValue: number | null;
   lengthValue: number | null;
   nameValue: string | undefined;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.data = this.getLocalStorage();
+  }
 
   get diagnostic() {
     return JSON.stringify(this.model);
@@ -37,15 +42,32 @@ export class HeroFormComponent implements OnInit {
   onLengthChange(length: number | null) {
     this.lengthValue = length;
   }
+
   onNameChange(name: string) {
     this.nameValue = name;
   }
+
   onSaveClick() {
     this.submitted = true;
     this.model.id += 1;
+    this.model.name = this.nameValue;
     this.model.area = this.areaValue;
     this.model.length = this.lengthValue;
     this.model.width = this.widthValue;
     this.model.unit = this.unitValue;
+    this.setLocalStorage();
+    this.model = new Hero(this.model.id, "Area Calculator", 0, 0, "Feet", 0);
+  }
+
+  setLocalStorage() {
+    this.data.push(this.model);
+    localStorage.setItem("myData", JSON.stringify(this.data));
+  }
+
+  getLocalStorage() {
+    var rawData = localStorage.getItem("myData");
+    var data = JSON.parse(rawData);
+    // this.data = data
+    return data;
   }
 }
